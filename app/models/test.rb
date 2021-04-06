ccf106cfcce0fbeba155323a451bfcbf99fcc8da
qagushaq <qwerty24164444@gmail.com>
2021-04-06 23:56:52 +0300

    fix_associations_name

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,8 +2,8 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: "User", foreign_key: "user_id"
   has_many :questions, dependent: :destroy
-  has_many :tests_users, dependent: :destroy
-  has_many :users, through: :tests_users, dependent: :destroy
+  has_many :test_user, dependent: :destroy
+  has_many :users, through: :test_user, dependent: :destroy
 
   scope :easy, -> { where(level: 0..1).order(created_at: :desc) }
   scope :medium, -> { where(level: 2..1).order(created_at: :desc) }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :tests_users
-  has_many :tests, through: :tests_users, dependent: :destroy
+  has_many :test_user
+  has_many :tests, through: :test_user, dependent: :destroy
   has_many :authored_tests, class_name: 'Test', dependent: :nullify
 
   validates :name, presence: true, uniqueness: true
