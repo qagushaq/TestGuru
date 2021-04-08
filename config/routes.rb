37087c988575dd546e7093533724c571cb7c32f3
qagushaq <qwerty24164444@gmail.com>
2021-04-08 17:24:27 +0300

    lesson8prev

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  
+
+  resources :answers
   root 'tests#index'
 
   resources :tests do
-    resources :questions, except: :index, shallow: true
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true
+    end
   end
 end
