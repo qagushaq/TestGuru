26231268e8efaeba101020e094ae61e105588d03
qagushaq <qwerty24164444@gmail.com>
2021-04-12 11:42:54 +0300

    pravki

--- db/migrate/20210408185753_drop_table_tests_users.rb
+++ db/migrate/20210408185753_drop_table_tests_users.rb
@@ -2,4 +2,8 @@ class DropTableTestsUsers < ActiveRecord::Migration[6.0]
   def up
     drop_table :tests_users, if_exists: true
   end
+
+  def down
+  end
+  
 end
--- db/migrate/20210408190342_drop_table_test_users.rb
+++ db/migrate/20210408190342_drop_table_test_users.rb
@@ -2,4 +2,8 @@ class DropTableTestUsers < ActiveRecord::Migration[6.0]
   def up
     drop_table :test_users, if_exists: true
   end
+
+  def down
+  end
+  
 end
