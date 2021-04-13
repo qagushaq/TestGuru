e80be0c9bf8f6c680c90483540ebdcb6b3cca333
qagushaq <qwerty24164444@gmail.com>
2021-04-13 22:57:34 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,6 @@ Rails.application.routes.draw do
   get :signup, to: 'users#new'
   get :login, to: 'sessions#new'
 
-  resources :users, only: :create
-  resources :sessions, only: :create
-
   resources :tests do
     resources :questions, shallow: true, except: :index do
       resources :answers, shallow: true, except: :index
