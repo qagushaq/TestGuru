1288c8a65addb5691b8cc334dbc1f36a2bc7f208
qagushaq <qwerty24164444@gmail.com>
2021-04-15 23:22:52 +0300

    add_localesyml

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -20,7 +20,7 @@ class Admin::TestsController < Admin::BaseController
   def create
     @test = current_user.authored_tests.new(test_params)
     if @test.save
-      redirect_to admin_test_path @test
+      redirect_to admin_test_path @test, notice: t('.success')
     else
       render :new
     end
