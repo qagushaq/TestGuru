559748b42b2412dcb836462cd9fa03f9b7b803b8
qagushaq <qwerty24164444@gmail.com>
2021-04-19 09:57:13 +0300

    fin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
 
+  COLORS={alert: 'danger', notice: 'warning', greeting: 'success', goodbye: 'primary'}
+
   def current_year
     "#{Time.current.year.to_i}"
   end
@@ -8,4 +10,12 @@ module ApplicationHelper
     link_to 'TestGuru', "https://github.com/#{author}/#{repo}", target: '_blank'
   end
 
+  def flash_message(flag)
+    tag.p(flash[flag], class: "flash #{flag}") if flash[flag]
+    tag.span(flash[flag]) if flash[flag]
+  end
+
+  def bootstrap_color(flag)
+    "alert-#{COLORS[flag.to_sym]}"
+  end
 end
