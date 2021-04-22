f446a9cb5b163c32afeb85e189345c4c3415e725
qagushaq <qwerty24164444@gmail.com>
2021-04-22 12:08:08 +0300

    Удален scope из модели gist, отредактировано отображение admin/gists

--- app/controllers/admin/gists_controller.rb
+++ app/controllers/admin/gists_controller.rb
@@ -1,7 +1,7 @@
 class Admin::GistsController < Admin::BaseController
 
   def index
-    @gists = Gist.gists_with_question_url_email.order(created_at: :desc)
+    @gists = Gist.all.order(created_at: :desc)
   end
 
 end
--- app/models/gist.rb
+++ app/models/gist.rb
@@ -3,6 +3,4 @@ class Gist < ApplicationRecord
   belongs_to :question
   belongs_to :user
 
-  scope :gists_with_question_url_email, -> { select(:email, :question_id, :body, :gist_url).joins(:user, :question) }
-
 end
