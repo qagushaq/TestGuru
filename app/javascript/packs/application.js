91f94e1e441142210b2a633ca8afe077d5edc5ed
qagushaq <qwerty24164444@gmail.com>
2021-04-26 00:00:39 +0300

    Добавлена пояявления формы редактирования при нажатии на кнопку Edit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ require("@rails/activestorage").start()
 require("channels")
 require("utilities/sorting")
 require("utilities/password_confirmation_check")
+require("jquery")
+require ("utilities/form_inline.js")
 
 
 
