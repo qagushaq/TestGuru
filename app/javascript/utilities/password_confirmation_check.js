c95f0dc09dbff51513f14fde5617a03439b7013c
qagushaq <qwerty24164444@gmail.com>
2021-04-25 13:55:43 +0300

    Убраны вложенные if

--- app/javascript/utilities/password_confirmation_check.js
+++ app/javascript/utilities/password_confirmation_check.js
@@ -5,25 +5,36 @@ document.addEventListener('turbolinks:load', function() {
   if (control) { control.addEventListener('input', checkPasswordConfirmation) }
 })
 
+let matchIcon = document.querySelector('.confirm')
+let noMatchIcon = document.querySelector('.notconfirm')
+
+function confirm() {
+  matchIcon.classList.remove('hide')
+  noMatchIcon.classList.add('hide')
+}
+
+function notconfirm() {
+  matchIcon.classList.add('hide')
+  noMatchIcon.classList.remove('hide')
+}
+
+function hide() {
+  matchIcon.classList.add('hide')
+  noMatchIcon.classList.add('hide')
+}
+
 function checkPasswordConfirmation() {
   let password = document.querySelector('#user_password').value
   let passwordConfirmation = document.querySelector('#user_password_confirmation').value
 
-  let matchIcon = document.querySelector('.confirm')
-  let noMatchIcon = document.querySelector('.notconfirm')
-
   if (passwordConfirmation.length > 0) {
-    if (password == passwordConfirmation) {
-      matchIcon.classList.remove('hide')
-      noMatchIcon.classList.add('hide')
-    }
-    else {
-      matchIcon.classList.add('hide')
-      noMatchIcon.classList.remove('hide')
-    }
+    return hide()
   }
-  else {
-    matchIcon.classList.add('hide')
-    noMatchIcon.classList.add('hide')
+
+  if (password == passwordConfirmation) {
+      confirm()
+    else {
+      notconfirm()
+    }
   }
 }
