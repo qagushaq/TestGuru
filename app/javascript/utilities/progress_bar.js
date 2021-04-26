b9f0286b5e7a4ddf88fbef30f292dc9ea007b679
qagushaq <qwerty24164444@gmail.com>
2021-04-26 10:45:16 +0300

    Упрощена логика работы прогресс-бара

--- app/javascript/utilities/progress_bar.js
+++ app/javascript/utilities/progress_bar.js
@@ -1,15 +1,9 @@
 document.addEventListener('turbolinks:load', function () {
 
- let progressBar = document.querySelector('.progress-bar')
+let progressDiv = document.querySelector('.progress');
+let progressBar = document.querySelector('.progress-bar')
 
- if (progressBar) {
-   showProgress(progressBar)
- }
-})
-
-function showProgress(progressBar) {
-  let totalQuestions = progressBar.dataset.totalQuestions
-  let currentQuestionNumber = progressBar.dataset.questionNumber
-  let percent = Math.round((currentQuestionNumber/totalQuestions)*100)
-  progressBar.style.width = `${percent}%`
+if (progressDiv && progressBar) {
+    progressBar.style.width = `${progressDiv.dataset.percent}%`
 }
+});
