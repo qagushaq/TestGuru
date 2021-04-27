c433fbf13eec94e9abfea5556bc35835b42000cb
qagushaq <qwerty24164444@gmail.com>
2021-04-27 13:24:33 +0300

    fix mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "test_guru_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'qagushaq.herokuapp.com }
+  config.action_mailer.default_url_options = { host: 'qagushaq.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
