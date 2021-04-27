1d4516a80cb67478a897e9a057d1017db0234c6d
qagushaq <qwerty24164444@gmail.com>
2021-04-27 16:12:30 +0300

    Поправлены редиректы

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -20,7 +20,7 @@ class Admin::QuestionsController < Admin::BaseController
   def create
     @question = @test.questions.new(question_params)
     if @question.save
-      redirect_to action: :index
+      redirect_to admin_test_path(@question.test)
     else
       render :new
     end
@@ -28,7 +28,7 @@ class Admin::QuestionsController < Admin::BaseController
 
   def update
     if @question.update(question_params)
-      redirect_to action: :index
+      redirect_to admin_test_path(@question.test)
     else
       render :edit
     end
@@ -36,7 +36,7 @@ class Admin::QuestionsController < Admin::BaseController
 
   def destroy
     @question.destroy
-    redirect_to action: :index
+    redirect_to admin_test_path(@question.test)
   end
 
   private
