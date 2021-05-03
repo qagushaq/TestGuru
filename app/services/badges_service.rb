98f3db7494f566ad19195f1ed20a07f95cd270d0
qagushaq <qwerty24164444@gmail.com>
2021-05-03 15:34:31 +0300

    Добавлен UserBadges контроллер и бейдж-сервис.

---
+++ app/services/badges_service.rb
@@ -0,0 +1,22 @@
+class BadgesService
+
+  def initialize(test_passage)
+    @test_passage = test_passage
+    @user = test_passage.user
+    @test = test_passage.test
+  end
+
+  def all_tests_category_complete?
+    category = @test.category
+    TestPassage.where(user: @user, test: @test).select(&:completed?).map(&:test_id).uniq.count == category.tests.count
+  end
+
+  def success_first_try?
+    @test_passage.completed? && @user.tests.where(title: @test_passage.test.title).count == 1
+  end
+
+  def all_tests_level_complete?
+    level = @test.level
+    @test_passage.completed? && @user.tests.where(level: level).uniq.count == Test.where(level: level).count
+  end
+end
