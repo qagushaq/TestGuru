8d365cef6d044135b6972a110c8f4996a7f5b652
qagushaq <qwerty24164444@gmail.com>
2021-05-04 18:12:00 +0300

    Поправлены связи, изменены названия правил

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,13 +1,14 @@
 class Badge < ApplicationRecord
 
-  has_many :user_badges
+  has_many :user_badges, dependent: :destroy
   has_many :users, through: :user_badges
+
   validates :title, :image, :rule, presence: true
 
   BADGE_RULES = {
-      'Passing all tests in a category' => :all_tests_category_complete,
+      'Passing all tests in a category' => :category_completed,
       'Passing the test successfully on the first try' => :success_first_try,
-      'Passing all tests of a certain level' => :all_tests_level_complete
+      'Passing all tests of a certain level' => :level_complete
   }.freeze
 
   def self.images
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,11 @@ class User < ApplicationRecord
          :validatable,
          :confirmable
 
-  has_many :test_passages
+  has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages, dependent: :destroy
   has_many :authored_tests, class_name: 'Test', foreign_key: :user_id
+  has_many :user_badges, dependent: :destroy
   has_many :badges, through: :user_badges
-  has_many :user_badges
 
   def tests_by_level(level)
     tests.where(level: level)
